d07105a8d30665dd653dcba5efffec7cd5302d04
Ankur Sharma <ankusharmapan125@gmail.com>
2025-08-03 21:23:05 +0530

    create student registration page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,14 @@ import { Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import AdminLoginPage from './pages/AdminLoginPage';
 import AdminDashboard from './pages/AdminDashboard'; 
+import StudentRegisterPage from './pages/StudentRegisterPage';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<AdminLoginPage />} />
+        <Route path="/admin-login" element={<AdminLoginPage />} />
         <Route path="/admin/dashboard" element={<AdminDashboard />} />
+        <Route path="/student-register" element={<StudentRegisterPage />} />
     </Routes>
   );
 }
